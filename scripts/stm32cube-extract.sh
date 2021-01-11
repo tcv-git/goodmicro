7cf9a0accad2e5b4fe5e5a4410023bd865fdbb44
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2021-01-11 20:51:39 +0000

    Add header to scripts

--- scripts/stm32cube-extract.sh
+++ scripts/stm32cube-extract.sh
@@ -1,5 +1,26 @@
 #! /bin/bash -eu
 
+# stm32cube-extract.sh
+# PUBLIC DOMAIN
+# http:///www.purposeful.co.uk/software/goodmicro
+
+
+# I, Tom Vajzovic, am the author of this software and its documentation.
+# I permanently abandon all copyright and other intellectual property rights
+# in them.
+#
+# I am fairly certain that the software does what the documentation says it
+# does, but I do not guarantee that it does, or that it does what you think it
+# should.  I do not guarantee that it will not have undesirable side effects.
+#
+# If you use, modify or distribute this software then you do so at your own
+# risk.  If you do not pass on this warning then you may be responsible for any
+# problems encountered by those who obtain the software through you.
+
+
+# script to extract parts of STM32Cube packages used in goodmicro repository
+
+
 copyto(){
 
   dest="$1"
