d946fe6584fae94cd267a873bebc01379f529469
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2021-01-12 19:33:53 +0000

    Add _exit assembler units for ARMv6M and ARMv7M

---
+++ lib/goodmicro_armv7m/_exit_reset.s
@@ -0,0 +1,38 @@
+@; _exit_reset.s
+@; PUBLIC DOMAIN
+@; http:///www.purposeful.co.uk/software/goodmicro
+
+
+@; I, Tom Vajzovic, am the author of this software and its documentation.
+@; I permanently abandon all intellectual property rights in them, including
+@; copyright, trademarks, design rights, database right, patents, and the right
+@; to be identified as the author.
+
+@; I am fairly certain that the software does what the documentation says it
+@; does, but I do not guarantee that it does, or that it does what you think it
+@; should.  I do not guarantee that it will not have undesirable side effects.
+
+@; You are free to use, modify and distribute this software as you please, but
+@; you do so at your own risk.  If you do not pass on this warning then you may
+@; be responsible for any problems encountered by those who obtain the software
+@; through you.
+
+
+.syntax         unified
+.cpu            cortex-m3
+
+.section        .text._exit
+.global         _exit
+.type           _exit, %function
+.thumb_func
+
+_exit:
+        cpsid   f
+        ldr     r0, =0xE000ED0C
+        ldr     r1, =0x05FA0704
+        dsb
+1:
+        str     r1, [r0]
+        b       1b
+
+.size   _exit, . - _exit
