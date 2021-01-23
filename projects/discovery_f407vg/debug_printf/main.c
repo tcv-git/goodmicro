27a3192c723535c59a5461515699807933d1c02b
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2021-01-23 08:07:07 +0000

    Add debug_printf for disco_f407vg

---
+++ projects/discovery_f407vg/debug_printf/main.c
@@ -0,0 +1,35 @@
+// main.c
+// PUBLIC DOMAIN
+// http:///www.purposeful.co.uk/software/goodmicro
+
+/*
+  I, Tom Vajzovic, am the author of this software and its documentation.
+  I permanently abandon all intellectual property rights in them, including
+  copyright, trademarks, design rights, database right, patents, and the right
+  to be identified as the author.
+
+  I am fairly certain that the software does what the documentation says it
+  does, but I do not guarantee that it does, or that it does what you think it
+  should.  I do not guarantee that it will not have undesirable side effects.
+
+  You are free to use, modify and distribute this software as you please, but
+  you do so at your own risk.  If you do not pass on this warning then you may
+  be responsible for any problems encountered by those who obtain the software
+  through you.
+*/
+
+#include "debug_printf.h"
+#include "delay.h"
+
+int main(void)
+{
+  debug_uart_init();
+
+  unsigned int i;
+
+  for (i = 0;; i++)
+  {
+    debug_printf("%u\n", i);
+    DELAY_MS(100);
+  }
+}
