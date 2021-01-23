1b082b007eb565ba477b16ae003d4d8b7063b277
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2021-01-23 12:55:25 +0000

    Add unit test for udiv64i

---
+++ lib/goodmicro_f4/hwrand.h
@@ -0,0 +1,35 @@
+/* hwrand.h PUBLIC DOMAIN 2019 t.cortexm@purposeful.co.uk */
+
+/* http:///www.purposeful.co.uk/software/cortexm */
+
+/*
+  I, Tom Vajzovic, am the author of this software and its documentation.
+  I permanently abandon all intellectual property rights in them, including
+  copyright, trademarks, design rights, database right, patents, and the right
+  to be identified as the author.
+
+  I am fairly certain that the software does what the documentation says it
+  does, but I do not guarantee that it does, or that it does what you think it
+  should.  I do not guarantee that it will not have undesirable side effects.
+
+  You are free to use, modify and distribute this software as you please, but
+  you do so at your own risk.  If you do not pass on this warning then you may
+  be responsible for any problems encountered by those who obtain the software
+  through you.
+*/
+
+#ifndef HWRAND_H_INCLUDED
+#define HWRAND_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void         hwrand_init (void);
+unsigned int hwrand32    (void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HWRAND_H_INCLUDED */
