c89d97ba4d98e493cfd2ea98e9abdf585565fa89
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2021-02-05 15:18:40 +0000

    Update unit tests for changes to delay units

--- projects/discovery_f407vg/test_delay_84MHz/test_delay_84MHz.s
+++ projects/discovery_f407vg/test_delay_84MHz/test_delay_84MHz.s
@@ -16,9 +16,10 @@
 @; problems encountered by those who obtain the software through you.
 
 
+@;##############################################################################
 @; delay functions using the debug cycle counter
 @; assuming fixed system core clock of 84 MHz
-
+@;##############################################################################
 
 .syntax unified
 .cpu    cortex-m3
@@ -29,76 +30,90 @@
 .global   delay_us
 .global   delay_ns
 
-.type     delay_s          , %function
-.type     delay_ms         , %function
-.type     delay_us         , %function
-.type     delay_ns         , %function
+.type     delay_s            , %function
+.type     delay_ms           , %function
+.type     delay_us           , %function
+.type     delay_ns           , %function
 
 
+@;##############################################################################
 .section  .text.delay_s
-.thumb_func
+@;##############################################################################
 
+.thumb_func
 delay_s:
-        ldr r3, =0xE0001004
-        ldr r1, [r3]
-        ldr r2, =84000000
+        ldr   r3, =0xE0001004
+        ldr   r1, [r3]
+        ldr   r2, =84000000
         umull r0, r12, r0, r2
-        b.n delay_coreclk_long_inner
+        b.n   delay_coreclk_long_inner
 
-.size delay_s, .-delay_s
+.size delay_s, . - delay_s
 
 
+@;##############################################################################
 .section  .text.delay_ms
-.thumb_func
+@;##############################################################################
 
+.thumb_func
 delay_ms:
-        ldr r3, =0xE0001004
-        ldr r1, [r3]
-        ldr r2, =84000
+        ldr   r3, =0xE0001004
+        ldr   r1, [r3]
+        ldr   r2, =84000
         umull r0, r12, r0, r2
-        b.n delay_coreclk_long_inner
+        b.n   delay_coreclk_long_inner
 
-.size delay_ms, .-delay_ms
+.size delay_ms, . - delay_ms
 
 
+@;##############################################################################
 .section  .text.delay_us
-.thumb_func
+@;##############################################################################
 
+.thumb_func
 delay_us:
-        ldr r3, =0xE0001004
-        ldr r1, [r3]
-        movs r2, 84
+        ldr   r3, =0xE0001004
+        ldr   r1, [r3]
+        movs  r2, 84
         umull r0, r12, r0, r2
-        b.n delay_coreclk_long_inner
+        b.n   delay_coreclk_long_inner
 
-.size delay_us, .-delay_us
+.size delay_us, . - delay_us
 
 
+@;##############################################################################
 .section  .text.delay_ns
-.thumb_func
+@;##############################################################################
+
 @; delay_ns: := delay_coreclk_long (      r0 * 0.084    )
 @;           := delay_coreclk_long (ceil (r0 * 21 / 250))
+.thumb_func
 delay_ns:
         ldr   r3, =0xE0001004
         ldr   r1, [r3]
-        push {r1, r3, lr}
+        push  {r1, r3, lr}
         movs  r1, 21
         umull r0, r1, r0, r1
         ldr   r3, =17179869  @; 2^32 / 250
         adds  r0, 249
         adcs  r1, 0
         movs  r2, 250
-        bl udiv64i    @; call not aeabi compliant beacuse stack not aligned here (I know udiv64i doesn't mind)
-        mov  r12, r1
-        pop  {r1, r3, lr}
-        b.n delay_coreclk_long_inner
+        bl    udiv64i  @; call not aeabi compliant beacuse stack not aligned here (I know udiv64i doesn't mind)
+        mov   r12, r1
+        pop   {r1, r3, lr}
+        b.n   delay_coreclk_long_inner
 
-.size delay_ns, .-delay_ns
+.size delay_ns, . - delay_ns
 
 
+@;##############################################################################
 .section  .text.delay_coreclk_long
-.thumb_func
+@;##############################################################################
 
+.thumb_func
 delay_coreclk_long_inner:
         mov r1, r12
         bx lr
+
+
+@;##############################################################################
--- projects/discovery_f407vg/test_delay_dynamic/test_delay_dynamic.s
+++ projects/discovery_f407vg/test_delay_dynamic/test_delay_dynamic.s
@@ -16,8 +16,9 @@
 @; problems encountered by those who obtain the software through you.
 
 
-@; delay functions using the SystemCoreClock variable and the debug cycle counter
-
+@;##############################################################################
+@; delay functions using the debug cycle counter and the SystemCoreClock variable
+@;##############################################################################
 
 .syntax unified
 .cpu    cortex-m3
@@ -27,110 +28,112 @@
 .global   delay_ms
 .global   delay_us
 .global   delay_ns
-.global   delay_coreclk
-.global   delay_coreclk_long
 
 .type     delay_s            , %function
 .type     delay_ms           , %function
 .type     delay_us           , %function
 .type     delay_ns           , %function
-.type     delay_coreclk      , %function
-.type     delay_coreclk_long , %function
-
-@ void delay_s            (unsigned int)
-@ void delay_ms           (unsigned int)
-@ void delay_us           (unsigned int)
-@ void delay_ns           (unsigned int)
-@ void delay_coreclk      (unsigned int)
-@ void delay_coreclk_long (unsigned long long int)
 
 
+@;##############################################################################
 .section  .text.delay_s
-.thumb_func
+@;##############################################################################
 
+.thumb_func
 delay_s:
-        ldr r3, =0xE0001004
-        ldr r1, [r3]
-        ldr r2, =SystemCoreClock
-        ldr r2, [r2]
+        ldr   r3, =0xE0001004
+        ldr   r1, [r3]
+        ldr   r2, =SystemCoreClock
+        ldr   r2, [r2]
         umull r0, r12, r0, r2
-        b.n delay_coreclk_long_inner
+        b.n   delay_coreclk_long_inner
 
-.size delay_s, .-delay_s
+.size delay_s, . - delay_s
 
 
+@;##############################################################################
 .section  .text.delay_ms
-.thumb_func
+@;##############################################################################
 
+.thumb_func
 delay_ms:
         ldr   r3, =0xE0001004
         ldr   r1, [r3]
-        push {r1, r3, lr}
+        push  {r1, r3, lr}
         ldr   r2, =SystemCoreClock
-        ldr   r3, =4294967 @; ((1 << 32) / 1000)
+        ldr   r3, =4294967  @; ((1 << 32) / 1000)
         ldr   r1, [r2]
         umull r0, r1, r0, r1
         movw  r2, 999
         adds  r0, r2
         adcs  r1, 0
         adds  r2, 1
-        bl udiv64i    @; call not aeabi compliant beacuse stack not aligned here (I know udiv64i doesn't mind)
+        bl    udiv64i  @; call not aeabi compliant beacuse stack not aligned here (I know udiv64i doesn't mind)
         mov   r12, r1
-        pop  {r1, r3, lr}
+        pop   {r1, r3, lr}
         b.n delay_coreclk_long_inner
 
-.size delay_ms, .-delay_ms
+.size delay_ms, . - delay_ms
 
 
+@;##############################################################################
 .section  .text.delay_us
-.thumb_func
+@;##############################################################################
 
+.thumb_func
 delay_us:
         ldr   r3, =0xE0001004
         ldr   r1, [r3]
-        push {r1, r3, lr}
+        push  {r1, r3, lr}
         ldr   r3, =SystemCoreClock
         ldr   r2, =999999
         ldr   r1, [r3]
-        movw  r3, 4294 @; ((1 << 32) / 1000000)
         umull r0, r1, r0, r1
+        movw  r3, 4294  @; ((1 << 32) / 1000000)
         adds  r0, r2
         adcs  r1, 0
         adds  r2, 1
-        bl udiv64i    @; call not aeabi compliant beacuse stack not aligned here (I know udiv64i doesn't mind)
+        bl    udiv64i  @; call not aeabi compliant beacuse stack not aligned here (I know udiv64i doesn't mind)
         mov   r12, r1
-        pop  {r1, r3, lr}
+        pop   {r1, r3, lr}
         b.n delay_coreclk_long_inner
 
-.size delay_us, .-delay_us
+.size delay_us, . - delay_us
 
 
+@;##############################################################################
 .section  .text.delay_ns
-.thumb_func
+@;##############################################################################
 
+.thumb_func
 delay_ns:
         ldr   r3, =0xE0001004
         ldr   r1, [r3]
-        push {r1, r3, lr}
+        push  {r1, r3, lr}
         ldr   r3, =SystemCoreClock
         ldr   r2, =999999999
         ldr   r1, [r3]
         umull r0, r1, r0, r1
-        movs  r3, 4 @; ((1 << 32) / 1000000000)
+        movs  r3, 4  @; ((1 << 32) / 1000000000)
         adds  r0, r2
         adcs  r1, 0
         adds  r2, 1
-        bl udiv64i    @; call not aeabi compliant beacuse stack not aligned here (I know udiv64i doesn't mind)
+        bl    udiv64i  @; call not aeabi compliant beacuse stack not aligned here (I know udiv64i doesn't mind)
         mov   r12, r1
-        pop  {r1, r3, lr}
+        pop   {r1, r3, lr}
         b.n delay_coreclk_long_inner
 
-.size delay_ns, .-delay_ns
+.size delay_ns, . - delay_ns
 
 
+@;##############################################################################
 .section  .text.delay_coreclk_long
-.thumb_func
+@;##############################################################################
 
+.thumb_func
 delay_coreclk_long_inner:
         mov r1, r12
         bx lr
+
+
+@;##############################################################################
