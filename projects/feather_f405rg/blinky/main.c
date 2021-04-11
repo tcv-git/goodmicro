ab5359dd8a787fb026a59737690046ed75dfc8e9
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2021-04-11 21:29:09 +0100

    Add feather board system_config.h and blinky project

---
+++ projects/feather_f405rg/blinky/main.c
@@ -0,0 +1,38 @@
+// main.c
+// PUBLIC DOMAIN
+// https://www.purposeful.co.uk/goodmicro/
+
+/*
+  I, Tom Vajzovic, am the author of this software and its documentation.
+  I permanently abandon all intellectual property rights in them, including
+  copyright, trademarks, design rights, database right, patents, and the right
+  to be identified as the author.
+
+  I am fairly certain that the software does what the documentation says it
+  does, but I do not guarantee that it does, or that it does what you think it
+  should.  I do not guarantee that it will not have undesirable side effects.
+
+  You are free to use, modify and distribute this software as you please, but
+  you do so at your own risk.  If you do not pass on this warning then you may
+  be responsible for any problems encountered by those who obtain the software
+  through you.
+*/
+
+#include "stm32f4xx_simple_gpio.h"
+#include "delay.h"
+
+int main(void)
+{
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+  (void)RCC->AHB1ENR;
+
+  GPIO_output_push_pull_slow(GPIOC, PIN1);
+
+  for (;;)
+  {
+    GPIO_set_reset(GPIOC, PIN1_HI);
+    DELAY_MS(350);
+    GPIO_set_reset(GPIOC, PIN1_LO);
+    DELAY_MS(650);
+  }
+}
