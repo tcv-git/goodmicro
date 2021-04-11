ee377db801ed48c551a8f30e13e4c98b1e886b91
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2021-04-11 09:14:05 +0100

    Add goodmicro_printf_queue

---
+++ lib/goodmicro_printf_queue/printf_queue.c
@@ -0,0 +1,123 @@
+// printf_queue.c
+// PUBLIC DOMAIN
+// https://www.purposeful.co.uk/software/goodmicro
+
+/*
+  I, Tom Vajzovic, am the author of this software and its documentation.
+  I permanently abandon all copyright and other intellectual property rights
+  in them.
+
+  I am fairly certain that the software does what the documentation says it
+  does, but I do not guarantee that it does, or that it does what you think it
+  should.  I do not guarantee that it will not have undesirable side effects.
+
+  If you use, modify or distribute this software then you do so at your own
+  risk.  If you do not pass on this warning then you may be responsible for any
+  problems encountered by those who obtain the software through you.
+*/
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include "bitpool.h"
+#include "bitqueue.h"
+#include "printf_queue.h"
+
+
+#define BUFFER_SIZE  64 // any size up to UINT16_MAX
+#define BUFFER_COUNT  8 // can be up to 16, but only 8 can be queued at once
+
+static char buffers[BUFFER_COUNT][BUFFER_SIZE];
+
+static volatile unsigned int buffers_available = ((1uLL << BUFFER_COUNT) - 1);
+static volatile unsigned int buffer_queue      = BITQUEUE_EMPTY_INIT;
+
+
+/* works like vprintf
+ */
+void printf_queue_put(const char *format, va_list args)
+{
+  int buffer_index = bitpool_alloc(&buffers_available);
+
+  if (buffer_index < 0)
+  {
+    return;
+  }
+
+  int len = vsnprintf(buffers[buffer_index], BUFFER_SIZE, format, args);
+
+  if (len < 1)
+  {
+    bitpool_free(&buffers_available, buffer_index);
+    return;
+  }
+
+  if (bitqueue_write(&buffer_queue, buffer_index) != 0)
+  {
+    bitpool_free(&buffers_available, buffer_index);
+    return;
+  }
+
+  return;
+}
+
+
+/* return a pointer to the oldest message queued
+ * or a null pointer if none available
+ * string is null-terminated
+ */
+char *printf_queue_get(void)
+{
+  int buffer_index = bitqueue_read(&buffer_queue);
+
+  if (buffer_index < 0)
+  {
+    return NULL;
+  }
+  else
+  {
+    return buffers[buffer_index];
+  }
+}
+
+
+/* return a pointer to the oldest message queued
+ * or a null pointer if none available
+ * string is not neccessarily null-terminated
+ * message length is output to *p_length
+ * \n at the end is replaced with \r\n
+ */
+char *printf_queue_get_crlf(uint16_t *p_length)
+{
+  char *buffer = printf_queue_get();
+
+  uint16_t length = strlen(buffer);
+
+  if ((length > 0) && (buffer[length - 1] == '\n'))
+  {
+    if ((length <= 1) || (buffer[length - 2] != '\r'))
+    {
+      buffer[length - 1] = '\r';
+      buffer[length++  ] = '\n';
+    }
+  }
+
+  *p_length = length;
+
+  return buffer;
+}
+
+
+/* free a buffer previously returned by printf_queue_get{,_crlf}
+ */
+void printf_queue_free(const char *buffer)
+{
+  if (buffer)
+  {
+    unsigned int buffer_index = ((buffer - &buffers[0][0]) / BUFFER_SIZE);
+
+    bitpool_free(&buffers_available, buffer_index);
+  }
+}
---
+++ projects/discovery_f429zi/printf_queue_test/main.c
@@ -0,0 +1,98 @@
+// main.c
+// PUBLIC DOMAIN
+// https://www.purposeful.co.uk/software/goodmicro
+
+/*
+  I, Tom Vajzovic, am the author of this software and its documentation.
+  I permanently abandon all copyright and other intellectual property rights
+  in them.
+
+  I am fairly certain that the software does what the documentation says it
+  does, but I do not guarantee that it does, or that it does what you think it
+  should.  I do not guarantee that it will not have undesirable side effects.
+
+  If you use, modify or distribute this software then you do so at your own
+  risk.  If you do not pass on this warning then you may be responsible for any
+  problems encountered by those who obtain the software through you.
+*/
+
+#include "delay.h"
+#include "debug_printf.h"
+#include "lcd.h"
+#include "lcd_text.h"
+#include "printf_queue.h"
+
+
+static void uart_puts(const char *str)
+{
+  while (*str)
+  {
+    debug_putc(*str++);
+  }
+}
+
+static int get_and_print(void)
+{
+  char *str = printf_queue_get();
+
+  if (str)
+  {
+    uart_puts(str);
+    printf_queue_free(str);
+  }
+
+  return !!str;
+}
+
+static void get_and_print_all(void)
+{
+  while (get_and_print());
+}
+
+static void get_and_print_n(unsigned int n)
+{
+  unsigned int i;
+
+  for (i = 0; i < n; i++)
+  {
+    get_and_print();
+  }
+}
+
+void test_printf(const char *format, ...) __attribute__((format(printf,1,2)));
+void test_printf(const char *format, ...)
+{
+  va_list args;
+
+  va_start(args, format);
+  printf_queue_put(format, args);
+  va_end(args);
+}
+
+int main (void)
+{
+  debug_uart_init();
+
+  lcd_init();
+  lcd_text_mask(0, 0, 40, 40);
+
+  unsigned int i = 0;
+
+  for (;; i++)
+  {
+    lcd_printf  ("%u\n", i);
+    test_printf ("%u\n", i);
+
+    DELAY_MS (250);
+
+    if ((i % 20) == 0)
+    {
+      get_and_print_n(3);
+    }
+
+    if (i > 200)
+    {
+      get_and_print_all();
+    }
+  }
+}
