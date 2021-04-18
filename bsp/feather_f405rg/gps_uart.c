8c6f8f415e8707ef598d62f95912e5eb9c8c7b33
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2021-04-18 22:06:35 +0100

    gps_uart sends setup message

--- bsp/feather_f405rg/gps_uart.c
+++ bsp/feather_f405rg/gps_uart.c
@@ -20,6 +20,7 @@
 #include "stm32f4xx_it.h"
 #include "stm32f4xx_simple_gpio.h"
 #include "system_stm32f405-439.h"
+#include "delay.h"
 #include "gps_uart.h"
 #include "gps_protocol.h"
 
@@ -28,8 +29,23 @@ PB10 AF7 USART3 TX  (GPS RX)
 PB11 AF7 USART3 RX  (GPS TX)
 */
 
-#define BAUD  9600
+#define INITIAL_BAUD   9600
+#define FINAL_BAUD   115200
 
+#define BAUD_MESSAGE  "\r\n\r\n$PMTK251,115200*1F\r\n"
+#define SETUP_MESSAGE  "\r\n$PMTK314,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n$PMTK220,100*2F\r\n"
+
+static void send_message(const char *message, unsigned int length)
+{
+  for (unsigned int i = 0; i < length; i++)
+  {
+    while ((USART3->SR & USART_SR_TXE) != USART_SR_TXE);
+
+    USART3->DR = message[i];
+  }
+
+  while ((USART3->SR & USART_SR_TC) != USART_SR_TC);
+}
 
 void gps_uart_init(void)
 {
@@ -37,12 +53,28 @@ void gps_uart_init(void)
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
   (void)RCC->AHB1ENR;
 
-  GPIO_alternate_push_pull_slow(GPIOB, PIN11, AF7);
+  GPIO_alternate_push_pull_slow(GPIOB, (PIN10 | PIN11), AF7);
 
   USART3->CR1 = 0;
   USART3->CR2 = 0;
   USART3->CR3 = 0;
-  USART3->BRR = ((APB1_CLK + (BAUD / 2)) / BAUD);
+  USART3->BRR = ((APB1_CLK + (INITIAL_BAUD / 2)) / INITIAL_BAUD);
+  USART3->SR  = 0;
+  USART3->CR1 = (USART_CR1_UE | USART_CR1_TE);
+
+  send_message(BAUD_MESSAGE, (sizeof BAUD_MESSAGE) - 1);
+
+  DELAY_MS(20);
+
+  USART3->CR1 = 0;
+  USART3->BRR = ((APB1_CLK + (FINAL_BAUD / 2)) / FINAL_BAUD);
+  USART3->SR  = 0;
+  USART3->CR1 = (USART_CR1_UE | USART_CR1_TE);
+
+  send_message(SETUP_MESSAGE, (sizeof SETUP_MESSAGE) - 1);
+
+  USART3->CR1 = 0;
+  USART3->SR  = 0;
   USART3->CR1 = (USART_CR1_UE | USART_CR1_RE | USART_CR1_RXNEIE);
 
   NVIC_EnableIRQ(USART3_IRQn);
