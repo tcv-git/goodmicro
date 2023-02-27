b6e7b9c0092e6e6515553a1a51033a097e4b60dd
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2023-02-27 21:25:16 +0000

    Add extracted vectors for STM32L4xx

---
+++ lib/goodmicro_stm32l4/stm32l4xx_it.h
@@ -0,0 +1,122 @@
+// stm32l4xx_it.h
+// PUBLIC DOMAIN
+// https://www.purposeful.co.uk/goodmicro/
+
+/*
+  I, Tom Vajzovic, am the author of this software and its documentation.
+  I permanently abandon all copyright and other intellectual property rights
+  in them.
+
+  I am fairly certain that the software does what the documentation says it
+  does, but I do not guarantee that it does, or that it does what you think it
+  should.  I do not guarantee that it will not have undesirable side effects.
+
+  If you use, modify or distribute this software then you do so at your own
+  risk.  If you do not pass on this warning then you may be responsible for any
+  problems encountered by those who obtain the software through you.
+*/
+
+#ifndef STM32L4XX_IT_H_INCLUDED
+#define STM32L4XX_IT_H_INCLUDED
+
+#ifdef STM32L412xx
+#include "vectors/vectors_stm32l412xx.h"
+#endif
+
+#ifdef STM32L422xx
+#include "vectors/vectors_stm32l422xx.h"
+#endif
+
+#ifdef STM32L431xx
+#include "vectors/vectors_stm32l431xx.h"
+#endif
+
+#ifdef STM32L432xx
+#include "vectors/vectors_stm32l432xx.h"
+#endif
+
+#ifdef STM32L433xx
+#include "vectors/vectors_stm32l433xx.h"
+#endif
+
+#ifdef STM32L442xx
+#include "vectors/vectors_stm32l442xx.h"
+#endif
+
+#ifdef STM32L443xx
+#include "vectors/vectors_stm32l443xx.h"
+#endif
+
+#ifdef STM32L451xx
+#include "vectors/vectors_stm32l451xx.h"
+#endif
+
+#ifdef STM32L452xx
+#include "vectors/vectors_stm32l452xx.h"
+#endif
+
+#ifdef STM32L462xx
+#include "vectors/vectors_stm32l462xx.h"
+#endif
+
+#ifdef STM32L471xx
+#include "vectors/vectors_stm32l471xx.h"
+#endif
+
+#ifdef STM32L475xx
+#include "vectors/vectors_stm32l475xx.h"
+#endif
+
+#ifdef STM32L476xx
+#include "vectors/vectors_stm32l476xx.h"
+#endif
+
+#ifdef STM32L485xx
+#include "vectors/vectors_stm32l485xx.h"
+#endif
+
+#ifdef STM32L486xx
+#include "vectors/vectors_stm32l486xx.h"
+#endif
+
+#ifdef STM32L496xx
+#include "vectors/vectors_stm32l496xx.h"
+#endif
+
+#ifdef STM32L4A6xx
+#include "vectors/vectors_stm32l4a6xx.h"
+#endif
+
+#ifdef STM32L4P5xx
+#include "vectors/vectors_stm32l4p5xx.h"
+#endif
+
+#ifdef STM32L4Q5xx
+#include "vectors/vectors_stm32l4q5xx.h"
+#endif
+
+#ifdef STM32L4R5xx
+#include "vectors/vectors_stm32l4r5xx.h"
+#endif
+
+#ifdef STM32L4R7xx
+#include "vectors/vectors_stm32l4r7xx.h"
+#endif
+
+#ifdef STM32L4R9xx
+#include "vectors/vectors_stm32l4r9xx.h"
+#endif
+
+#ifdef STM32L4S5xx
+#include "vectors/vectors_stm32l4s5xx.h"
+#endif
+
+#ifdef STM32L4S7xx
+#include "vectors/vectors_stm32l4s7xx.h"
+#endif
+
+#ifdef STM32L4S9xx
+#include "vectors/vectors_stm32l4s9xx.h"
+#endif
+
+#endif /* STM32L4XX_IT_H_INCLUDED */
