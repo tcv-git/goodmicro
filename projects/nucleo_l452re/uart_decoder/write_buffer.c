093d4241825fc9d557e0335f002c0d38ca941157
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2023-03-02 18:33:31 +0000

    Make write_buffer a singleton

--- projects/nucleo_l452re/uart_decoder/write_buffer.c
+++ projects/nucleo_l452re/uart_decoder/write_buffer.c
@@ -4,118 +4,132 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
 #include "uart_dma_write.h"
 #include "write_buffer.h"
 
-
-void write_buffer_init(struct write_buffer *wb, uint8_t *buffer, uint32_t buffer_size)
+struct state
 {
-  wb->buffer      = buffer;
-  wb->buffer_size = buffer_size;
-  wb->next_write  = 0;
-  wb->read_start  = 0;
-  wb->read_end    = 0;
-  wb->overflow    = false;
+  uint8_t *buffer;
+  uint32_t buffer_size;
+
+  uint32_t next_write;
+  uint32_t read_start;
+  uint32_t read_end;
+  bool     overflow;
+};
+
+static struct state state;
+
+void write_buffer_init(uint8_t *buffer, uint32_t buffer_size)
+{
+  state.buffer      = buffer;
+  state.buffer_size = buffer_size;
+  state.next_write  = 0;
+  state.read_start  = 0;
+  state.read_end    = 0;
+  state.overflow    = false;
 }
 
-void write_buffer_write(struct write_buffer *wb, const uint8_t *data, uint32_t data_count)
+void write_buffer_write(const uint8_t *data, uint32_t data_count)
 {
-  if (wb->overflow)
+  if (state.overflow)
   {
     return;
   }
 
-  if (wb->read_start == 0)
+  if (state.read_start == 0)
   {
-    uint32_t space = ((wb->buffer_size - 1) - wb->next_write);
+    uint32_t space = ((state.buffer_size - 1) - state.next_write);
 
     if (data_count <= space)
     {
-      memcpy(&wb->buffer[wb->next_write], data, data_count);
+      memcpy(&state.buffer[state.next_write], data, data_count);
 
-      wb->next_write += data_count;
+      state.next_write += data_count;
     }
     else
     {
-      wb->overflow = true;
+      state.overflow = true;
     }
   }
-  else if (wb->next_write >= wb->read_start)
+  else if (state.next_write >= state.read_start)
   {
-    uint32_t space_here = (wb->buffer_size - wb->next_write);
-    uint32_t total_space = (space_here + (wb->read_start - 1));
+    uint32_t space_here = (state.buffer_size - state.next_write);
+    uint32_t total_space = (space_here + (state.read_start - 1));
 
     if (data_count < space_here)
     {
-      memcpy(&wb->buffer[wb->next_write], data, data_count);
+      memcpy(&state.buffer[state.next_write], data, data_count);
 
-      wb->next_write += data_count;
+      state.next_write += data_count;
     }
     else if (data_count <= total_space)
     {
       uint32_t remainder = (data_count - space_here);
 
-      memcpy(&wb->buffer[wb->next_write], data, space_here);
+      memcpy(&state.buffer[state.next_write], data, space_here);
 
-      memcpy(wb->buffer, &data[space_here], remainder);
+      memcpy(state.buffer, &data[space_here], remainder);
 
-      wb->next_write = remainder;
+      state.next_write = remainder;
     }
     else
     {
-      wb->overflow = true;
+      state.overflow = true;
     }
   }
   else
   {
-    uint32_t space = ((wb->read_start - 1) - wb->next_write);
+    uint32_t space = ((state.read_start - 1) - state.next_write);
 
     if (data_count <= space)
     {
-      memcpy(&wb->buffer[wb->next_write], data, data_count);
+      memcpy(&state.buffer[state.next_write], data, data_count);
 
-      wb->next_write += data_count;
+      state.next_write += data_count;
     }
     else
     {
-      wb->overflow = true;
+      state.overflow = true;
     }
   }
 }
 
-void write_buffer_poll(struct write_buffer *wb)
+void write_buffer_poll(void)
 {
-  if (wb->read_end != wb->read_start)
+  if (state.read_end != state.read_start)
   {
     if (uart_write_complete())
     {
-      wb->read_start = wb->read_end;
+      state.read_start = state.read_end;
     }
   }
 
-  if (wb->overflow)
+  if (state.overflow)
   {
     uint8_t overflow_marker = '\v';
 
-    wb->overflow = false;
+    state.overflow = false;
 
-    write_buffer_write(wb, &overflow_marker, sizeof overflow_marker);
+    write_buffer_write(&overflow_marker, sizeof overflow_marker);
   }
 
-  if (wb->read_end != wb->read_start)
+  if (state.read_end != state.read_start)
   {
     return;
   }
 
-  if (wb->read_start < wb->next_write)
+  if (state.read_start < state.next_write)
   {
-    uart_write_start(&wb->buffer[wb->read_start], (wb->next_write - wb->read_start));
-    wb->read_end = wb->next_write;
+    uart_write_start(&state.buffer[state.read_start], (state.next_write - state.read_start));
+    state.read_end = state.next_write;
   }
-  else if (wb->read_start > wb->next_write)
+  else if (state.read_start > state.next_write)
   {
-    uart_write_start(&wb->buffer[wb->read_start], (wb->buffer_size - wb->read_start));
-    wb->read_end = 0;
+    uart_write_start(&state.buffer[state.read_start], (state.buffer_size - state.read_start));
+    state.read_end = 0;
   }
   else
   {
@@ -123,10 +137,32 @@ void write_buffer_poll(struct write_buffer *wb)
   }
 }
 
-void write_buffer_flush(struct write_buffer *wb)
+void write_buffer_printf(const char *format, ...)
 {
-  while ((wb->read_end != wb->read_start) || (wb->read_start != wb->next_write))
+  uint8_t buffer[128];
+  va_list args;
+
+  va_start(args, format);
+  int len = vsnprintf((char*)buffer, sizeof buffer, format, args);
+  va_end(args);
+
+  if (len < 0)
   {
-    write_buffer_poll(wb);
+    len = 0;
+  }
+
+  if ((size_t)len >= sizeof buffer)
+  {
+    len = ((sizeof buffer) - 1);
+  }
+
+  write_buffer_write(buffer, len);
+}
+
+void write_buffer_flush(void)
+{
+  while ((state.read_end != state.read_start) || (state.read_start != state.next_write))
+  {
+    write_buffer_poll();
   }
 }
--- projects/nucleo_l452re/uart_decoder/write_buffer.h
+++ projects/nucleo_l452re/uart_decoder/write_buffer.h
@@ -5,27 +5,16 @@
 #define WRITE_BUFFER_H_INCLUDED
 
 #include <stdint.h>
-#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-struct write_buffer
-{
-  uint8_t *buffer;
-  uint32_t buffer_size;
-
-  uint32_t next_write;
-  uint32_t read_start;
-  uint32_t read_end;
-  bool     overflow;
-};
-
-void write_buffer_init(struct write_buffer *wb, uint8_t *buffer, uint32_t buffer_size);
-void write_buffer_write(struct write_buffer *wb, const uint8_t *data, uint32_t data_count);
-void write_buffer_poll(struct write_buffer *wb);
-void write_buffer_flush(struct write_buffer *wb);
+void write_buffer_init(uint8_t *buffer, uint32_t buffer_size);
+void write_buffer_write(const uint8_t *data, uint32_t data_count);
+void write_buffer_printf(const char *format, ...) __attribute__((format(printf,1,2)));
+void write_buffer_poll(void);
+void write_buffer_flush(void);
 
 #ifdef __cplusplus
 }
