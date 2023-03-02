47b8760f2b941a9348ae48aad22752487005090e
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2023-03-02 19:52:18 +0000

    Implement event_queue.c

--- projects/nucleo_l452re/uart_decoder/event_queue.c
+++ projects/nucleo_l452re/uart_decoder/event_queue.c
@@ -1,96 +1,80 @@
+// Ring buffer for passing UART events
+// Copyright Green Energy Options Ltd.
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "event_queue.h"
 
-
-
-
-
-
-
-#if 0
-
-void usart_receiver_init(
-        struct usart_receiver *receiver,
-        uint8_t *buffer,
-        uint32_t buffer_size);
+struct state
 {
-  receiver->usart           = usart;
-  receiver->buffer          = buffer;
-  receiver->buffer_size     = buffer_size;
-  receiver->buffer_count    = 0;
-  receiver->last_rx_time    = clock_read();
-  receiver->intermcu_active = false;
+  uint16_t *buffer;
+  uint32_t buffer_count;
+  uint32_t next_write;
+  uint32_t next_read;
+  bool     overflow;
+};
 
+static struct state state;
+
+
+void event_queue_init(uint16_t *buffer, uint32_t buffer_count)
+{
+  state.buffer        = buffer;
+  state.buffer_count = buffer_count;
+  state.next_write    = 0;
+  state.next_read     = 0;
 }
 
-
-void read_buffer_add(int received_data, uint32_t timstamp)
+void event_queue_put(enum event_type type, uint_fast8_t channel, uint8_t data)
 {
-  if ((received_data == NO_DATA) || (received_data == TIMEOUT))
+  uint16_t event = event_pack(type, channel, data);
+
+  if (state.overflow)
   {
-    // recalculate in software whether we have timed out
-    // because not all UARTs support hardware timeout
-
-    uint32_t elapsed = (timstamp - receiver->last_rx_time);
-
-    received_data = ((elapsed > CLOCK_TIMEOUT) ? TIMEOUT : NO_DATA);
+    state.overflow = false;
+    event_queue_put(EVENT_MUX_OVERRUN, 0, 0);  // recurses a maximum of once!
   }
 
-  if (received_data >= 0)
+  uint32_t next_next_write = (state.next_write + 1);
+
+  if (next_next_write == state.buffer_count)
   {
-    buffer[buffer_count] = received_data;
-    buffer_count++;
+    next_next_write = 0;
   }
 
-  bool full = (buffer_count == buffer_size);
-
-
-
-
-
+  if (next_next_write == state.next_read)
+  {
+    state.overflow = true;
+  }
+  else
+  {
+    state.buffer[state.next_write] = event;
+    state.next_write = next_next_write;
+  }
 }
 
-
-
-
-void poll_receiver(struct usart_receiver *receiver)
+uint16_t event_queue_get(void)
 {
-  uint32_t now = time_now();
+  uint32_t next_read = state.next_read;
 
-  if (!(receiver->usart->SR & USART_SR_RXNE))
+  if (next_read == state.next_write)
   {
-    if ((now - receiver->last_rx_time) > BYTE_TIMEOUT)
+    return event_pack(EVENT_NO_DATA, 0, 0);
+  }
+  else
+  {
+    uint16_t event = state.buffer[next_read];
+
+    next_read++;
+
+    if (next_read == state.buffer_count)
     {
-      if (receiver->intermcu_active)
-      {
-        print_incomplete_intermcu(receiver->buffer, receiver->count);
-      }
-      else
-      {
-        print_non_intermcu(receiver->buffer, receiver->count);
-      }
-
-      receiver->count = 0;
+      next_read = 0;
     }
 
-    return;
-  }
+    state.next_read = next_read;
 
-  receiver->buffer[receiver->count++] = receiver->usart->RDR;
-
-  receiver->last_rx_time = now;
-
-  if (intermcu_message_complete(receiver->buffer, receiver->count))
-  {
-    intermcu_output_message(receiver->buffer, receiver->count);
-    receiver->count = 0;
+    return event;
   }
 }
 
-
-uint buffer_full(buffer, count);
-
-enum event {
-  new data,
-  timeout,
-
-#endif
--- projects/nucleo_l452re/uart_decoder/event_queue.h
+++ projects/nucleo_l452re/uart_decoder/event_queue.h
@@ -1,54 +1,54 @@
+// Ring buffer for passing UART events
+// Copyright Green Energy Options Ltd.
 
+#ifndef EVENT_QUEUE_H_INCLUDED
+#define EVENT_QUEUE_H_INCLUDED
 
+#include <stdint.h>
 
-#if 0
-#define MASK_EVENT    0xF000
-#define MASK_CHANNEL  0x0F00
-#define MASK_DATA     0x00FF
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 enum event_type
 {
   EVENT_NO_DATA, // events of this type aren't put in the queue but are returned if the queue is empty
-  EVENT_TIMEOUT,
+  EVENT_HW_TIMEOUT,
   EVENT_DATA,
-  EVENT_OVERRUN,
+  EVENT_MUX_OVERRUN, // events of this type aren't put in the queue but are returned if the queue overflowed
+  EVENT_HW_OVERRUN,
   EVENT_NOISE_ERROR,
   EVENT_FRAMING_ERROR,
   EVENT_PARITY_ERROR,
 };
 
-struct event_queue
+void     event_queue_init(uint16_t *buffer, uint32_t buffer_count);
+void     event_queue_put(enum event_type type, uint_fast8_t channel, uint8_t data);
+uint16_t event_queue_get(void);
+
+
+static inline __attribute__((always_inline)) uint16_t event_pack(enum event_type type, uint_fast8_t channel, uint8_t data)
 {
-  uint16_t *buffer;
-  uint32_t buffer_length;
-  uint32_t next_write;
-  uint32_t next_read;
-};
+  return ((type << 12) | ((channel & 0xF) << 8) | data);
+}
 
-void event_queue_init(struct event_queue *q, uint16_t *buffer, uint32_t buffer_length);
-void event_queue_put(struct event_queue *q, enum event_type event, uint8_t channel, uint8_t data);
-uint16_t event_queue_get(struct event_queue *q);
+static inline __attribute__((always_inline)) enum event_type event_get_type(uint16_t event)
+{
+  return (event >> 12);
+}
 
-enum event_type event_get_type(uint16_t packed_event);
-unsigned int event_get_channel(uint16_t packed_event);
-uint8_t event_get_data(uint16_t packed_event);
+static inline __attribute__((always_inline)) uint_fast8_t event_get_channel(uint16_t event)
+{
+  return ((event >> 8) & 0xF);
+}
+
+static inline __attribute__((always_inline)) uint8_t event_get_data(uint16_t event)
+{
+  return (event & 0xFF);
+}
+
+#ifdef __cplusplus
+}
 #endif
 
-
-
-#if 0
-
-struct usart_receiver
-{
-  uint8_t *buffer;
-  uint32_t buffer_size;
-  uint32_t buffer_count;
-  uint32_t last_rx_time;
-};
-
-void usart_receiver_init(struct usart_receiver *receiver, uint8_t *buffer, uint32_t buffer_size);
-void usart_receiver_poll(struct usart_receiver *receiver);
-
-void read_buffer_add(int received_data, uint32_t timstamp);
-
-#endif
+#endif // EVENT_QUEUE_H_INCLUDED
