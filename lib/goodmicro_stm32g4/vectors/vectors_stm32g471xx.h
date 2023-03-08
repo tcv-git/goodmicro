61a15934b39bd1dd3c93efffd6ce9b1f07517b00
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2023-03-08 18:10:47 +0000

    Add extracted vectors for STM32G4xx

---
+++ lib/goodmicro_stm32g4/vectors/vectors_stm32g471xx.h
@@ -0,0 +1,105 @@
+// vectors_stm32g471xx.h
+// automaically generated interrupt vector header
+
+#ifndef VECTORS_STM32G471XX_H_INCLUDED
+#define VECTORS_STM32G471XX_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void Reset_Handler(void);
+void NMI_Handler(void);
+void HardFault_Handler(void);
+void MemManage_Handler(void);
+void BusFault_Handler(void);
+void UsageFault_Handler(void);
+void SVC_Handler(void);
+void DebugMon_Handler(void);
+void PendSV_Handler(void);
+void SysTick_Handler(void);
+void WWDG_IRQHandler(void);
+void PVD_PVM_IRQHandler(void);
+void RTC_TAMP_LSECSS_IRQHandler(void);
+void RTC_WKUP_IRQHandler(void);
+void FLASH_IRQHandler(void);
+void RCC_IRQHandler(void);
+void EXTI0_IRQHandler(void);
+void EXTI1_IRQHandler(void);
+void EXTI2_IRQHandler(void);
+void EXTI3_IRQHandler(void);
+void EXTI4_IRQHandler(void);
+void DMA1_Channel1_IRQHandler(void);
+void DMA1_Channel2_IRQHandler(void);
+void DMA1_Channel3_IRQHandler(void);
+void DMA1_Channel4_IRQHandler(void);
+void DMA1_Channel5_IRQHandler(void);
+void DMA1_Channel6_IRQHandler(void);
+void DMA1_Channel7_IRQHandler(void);
+void ADC1_2_IRQHandler(void);
+void USB_HP_IRQHandler(void);
+void USB_LP_IRQHandler(void);
+void EXTI9_5_IRQHandler(void);
+void TIM1_BRK_TIM15_IRQHandler(void);
+void TIM1_UP_TIM16_IRQHandler(void);
+void TIM1_TRG_COM_TIM17_IRQHandler(void);
+void TIM1_CC_IRQHandler(void);
+void TIM2_IRQHandler(void);
+void TIM3_IRQHandler(void);
+void TIM4_IRQHandler(void);
+void I2C1_EV_IRQHandler(void);
+void I2C1_ER_IRQHandler(void);
+void I2C2_EV_IRQHandler(void);
+void I2C2_ER_IRQHandler(void);
+void SPI1_IRQHandler(void);
+void SPI2_IRQHandler(void);
+void USART1_IRQHandler(void);
+void USART2_IRQHandler(void);
+void USART3_IRQHandler(void);
+void EXTI15_10_IRQHandler(void);
+void RTC_Alarm_IRQHandler(void);
+void USBWakeUp_IRQHandler(void);
+void TIM8_BRK_IRQHandler(void);
+void TIM8_UP_IRQHandler(void);
+void TIM8_TRG_COM_IRQHandler(void);
+void TIM8_CC_IRQHandler(void);
+void ADC3_IRQHandler(void);
+void LPTIM1_IRQHandler(void);
+void TIM5_IRQHandler(void);
+void SPI3_IRQHandler(void);
+void UART4_IRQHandler(void);
+void UART5_IRQHandler(void);
+void TIM6_DAC_IRQHandler(void);
+void TIM7_IRQHandler(void);
+void DMA2_Channel1_IRQHandler(void);
+void DMA2_Channel2_IRQHandler(void);
+void DMA2_Channel3_IRQHandler(void);
+void DMA2_Channel4_IRQHandler(void);
+void DMA2_Channel5_IRQHandler(void);
+void UCPD1_IRQHandler(void);
+void COMP1_2_3_IRQHandler(void);
+void COMP4_IRQHandler(void);
+void CRS_IRQHandler(void);
+void SAI1_IRQHandler(void);
+void FPU_IRQHandler(void);
+void I2C4_EV_IRQHandler(void);
+void I2C4_ER_IRQHandler(void);
+void SPI4_IRQHandler(void);
+void RNG_IRQHandler(void);
+void LPUART1_IRQHandler(void);
+void I2C3_EV_IRQHandler(void);
+void I2C3_ER_IRQHandler(void);
+void DMAMUX_OVR_IRQHandler(void);
+void QUADSPI_IRQHandler(void);
+void DMA1_Channel8_IRQHandler(void);
+void DMA2_Channel6_IRQHandler(void);
+void DMA2_Channel7_IRQHandler(void);
+void DMA2_Channel8_IRQHandler(void);
+void CORDIC_IRQHandler(void);
+void FMAC_IRQHandler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // VECTORS_STM32G471XX_H_INCLUDED
