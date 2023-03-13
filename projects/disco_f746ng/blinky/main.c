d462059fa747d6833d3f93c5e8f16a02b52fe761
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2023-03-13 21:03:39 +0000

    Start work on blinky project for disco_f746ng (clocks not working yet)

---
+++ projects/disco_f746ng/blinky/main.c
@@ -0,0 +1,41 @@
+// main.c
+// PUBLIC DOMAIN
+// https://www.purposeful.co.uk/goodmicro/
+
+/*
+  I, Tom Vajzovic, am the author of this software and its documentation.
+  I permanently abandon all copyright and other intellectual property rights
+  in them.
+
+  I am fairly certain that the software does what the documentation says it
+  does, but I do not guarantee that it does, or that it does what you think it
+  should.  I do not guarantee that it will not have undesirable side effects.
+
+  If you use, modify or distribute this software then you do so at your own
+  risk.  If you do not pass on this warning then you may be responsible for any
+  problems encountered by those who obtain the software through you.
+*/
+
+#include "stm32f7xx_simple_gpio.h"
+#include "delay.h"
+
+int main(void)
+{
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOIEN;
+  (void)RCC->AHB1ENR;
+
+  GPIO_output_push_pull_slow(GPIOI, PIN1);
+  GPIO_input(GPIOI, PIN11);
+
+  for (;;)
+  {
+    if (GPIOI->IDR & PIN11)
+    {
+      GPIO_set_reset(GPIOI, PIN1_HI);
+    }
+    else
+    {
+      GPIO_set_reset(GPIOI, PIN1_LO);
+    }
+  }
+}
