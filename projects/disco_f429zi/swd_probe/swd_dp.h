976a209cabfbb44375d10ee1974492d9a353aad4
Tom Vajzovic <t.goodmicro@purposeful.co.uk>
2024-03-19 21:52:45 +0000

    SWD AP read access working

--- projects/disco_f429zi/swd_probe/swd_dp.h
+++ projects/disco_f429zi/swd_probe/swd_dp.h
@@ -11,25 +11,21 @@ enum result
   ERROR_RESPONSE,
   PROTOCOL_ERROR,
   PARITY_ERROR,
+  UNSUPPORTED_RESPONSE,
 };
 
 void reset_sequence(void);
 
-enum result read_dpidr    (uint32_t *p_data);
+enum result read_idcode   (uint32_t *p_data);
 enum result read_ctrlstat (uint32_t *p_data);
-enum result read_dlcr     (uint32_t *p_data);
+enum result read_wcr      (uint32_t *p_data);
 enum result read_resend   (uint32_t *p_data);
 enum result read_rdbuff   (uint32_t *p_data);
 
 enum result write_abort    (uint32_t data);
 enum result write_ctrlstat (uint32_t data);
-enum result write_dlcr     (uint32_t data);
+enum result write_wcr      (uint32_t data);
 enum result write_select   (uint32_t data);
 
-enum result set_apsel(uint8_t apsel);
-enum result set_apbank(uint8_t apbank);
-enum result set_dpbank(uint8_t dpbank);
-enum result set_apsel_apbank(uint8_t apsel, uint8_t apbank);
-enum result set_apsel_apbank_dbank(uint8_t apsel, uint8_t apbank, uint8_t dpbank);
-
 enum result read_ap(uint8_t apsel, uint8_t address, uint32_t *p_data);
+enum result write_ap(uint8_t apsel, uint8_t address, uint32_t data);
